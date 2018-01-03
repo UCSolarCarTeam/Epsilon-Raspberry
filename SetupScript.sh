f8ddbf053be10dace5e64ac45132b80b78122441
louisj381 <louisj381@gmail.com>
2018-01-02 22:41:10 -0700

    formatted setupscript in accordance to Enoch's comments

--- SetupScript.sh
+++ SetupScript.sh
@@ -2,13 +2,13 @@
 
 if [ $USER -ne "root"]
 	then
-	echo "Permission Denied"
+	echo "Please run as root user"
 	exit 1
 fi
 #base script to fresh install systems onto base image for raspberry pi
 
 #check internet connection
-ping -q -c3 google.com > /dev/null
+ping -q -c1 google.com > /dev/null
 
 if [ $? -ne 0 ]
 then
@@ -17,11 +17,8 @@ then
 fi
 
 #configure displays
-cd /opt/
-git clone https://github.com/UCSolarCarTeam/Epsilon-Raspberry.git
-cd Epsilon-Raspberry/primary/
-cp xorg.conf /etc/X11/
-cp config.txt /boot/
+(cd /opt/ && git clone https://github.com/UCSolarCarTeam/Epsilon-Raspberry.git)
+(cd /opt/Epsilon-Raspberry/primary/ && cp xorg.conf /etc/X11/ && cp config.txt /boot/)
 
 #install rabbit-mq
 echo 'deb http://www.rabbitmq.com/debian/ testing main' | tee /etc/apt/sources.list.d/rabbitmq.list && apt-get update && apt-get install rabbitmq-server
@@ -43,31 +40,72 @@ fi
 #begin QT install
 apt-get update
 apt-get --yes upgrade
-apt-get install --yes libfontconfig1-dev libdbus-1-dev libfreetype6-dev libudev-dev libicu-dev libsqlite3-dev
-libxslt1-dev libssl-dev libasound2-dev libavcodec-dev libavformat-dev libswscale-dev libgstreamer0.10-dev
-libgstreamer-plugins-base0.10-dev gstreamer-tools gstreamer0.10-plugins-good gstreamer0.10-plugins-bad
-libraspberrypi-dev libpulse-dev libx11-dev libglib2.0-dev libcups2-dev freetds-dev libsqlite0-dev libpq-dev
-libiodbc2-dev libmysqlclient-dev firebird-dev libpng12-dev libjpeg9-dev libgst-dev libxext-dev libxcb1 libxcb1-dev
-libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 libxcb-image0-dev libxcb-shm0
-libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-sync1 libxcb-sync-dev libxcb-render-util0
-libxcb-render-util0-dev libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-glx0-dev
-libxi-dev libdrm-dev libssl-dev libxcb-xinerama0 libxcb-xinerama0-dev
-
+apt-get install --yes -qq \
+	libfontconfig1-dev \
+	libdbus-1-dev \
+	libfreetype6-dev \
+	libudev-dev \
+	libicu-dev \
+	libsqlite3-dev \
+	libxslt1-dev \
+	libssl-dev \
+	libasound2-dev \
+	libavcodec-dev \
+	libavformat-dev \
+	libswscale-dev \
+	libgstreamer0.10-dev \
+	libgstreamer-plugins-base0.10-dev \
+	gstreamer-tools \
+	gstreamer0.10-plugins-good gstreamer0.10-plugins-bad \
+	libraspberrypi-dev \
+	libpulse-dev \
+	libx11-dev \
+	libglib2.0-dev \
+	libcups2-dev \
+	freetds-dev \
+	libsqlite0-dev \
+	libpq-dev \
+	libiodbc2-dev \
+	libmysqlclient-dev \
+	firebird-dev \
+	libpng12-dev \
+	libjpeg9-dev \
+	libgst-dev \
+	libxext-dev \
+	libxcb1 \
+	libxcb1-dev \
+	libx11-xcb1 \
+	libx11-xcb-dev \
+	libxcb-keysyms1 \
+	libxcb-keysyms1-dev \
+	libxcb-image0 \
+	libxcb-image0-dev \
+	libxcb-shm0 \
+	libxcb-shm0-dev \
+	libxcb-icccm4 \
+	libxcb-icccm4-dev \
+	libxcb-sync1 \
+	libxcb-sync-dev \
+	libxcb-render-util0 \
+	libxcb-render-util0-dev \
+	libxcb-xfixes0-dev \
+	libxrender-dev \
+	libxcb-shape0-dev \
+	libxcb-randr0-dev \
+	libxcb-glx0-dev \
+	libxi-dev \
+	libdrm-dev \
+	libssl-dev \
+	libxcb-xinerama0 \
+	libxcb-xinerama0-dev
 mkdir ~/opt
-cd ~/opt
-git clone git://code.qt.io/qt/qt5.git
-cd ~/Epsilon-Raspberry
-mv fix-initrepo.patch ~/opt
-cd qt5
-git checkout v5.5.1
-cd ..
-patch -Np1 -d qt5 < fix-initrepo.patch
-cd qt5
-perl init-repository -f
-cd ~/Epsilon-Raspberry
-mv QT_CFLAGS_DBUS.patch ~/opt/qt5
-cd ~/opt/qt5
-patch -Np1 -d qtbase < QT_CFLAGS_DBUS.patch
+(cd ~/opt && git clone git://code.qt.io/qt/qt5.git)
+(cd ~/Epsilon-Raspberry && mv fix-initrepo.patch ~/opt)
+(cd ~/opt/qt5 && git checkout v5.5.1)
+(cd ~/opt && patch -Np1 -d qt5 < fix-initrepo.patch)
+(cd ~/opt/qt5 && perl init-repository -f)
+(cd ~/Epsilon-Raspberry && mv QT_CFLAGS_DBUS.patch ~/opt/qt5)
+(cd ~/opt/qt5 && patch -Np1 -d qtbase < QT_CFLAGS_DBUS.patch)
 cd qtbase
 ./configure -v -opengl es2 -device linux-rasp-pi-g''+ -device-option CROSS_COMPILE=/usr/bin/ -opensource 
 -confirm-license -optimized-qmake -reduce-exports -release -qt-pcre -make libs -prefix /usr/local/qt5 &> output
