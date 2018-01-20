93604fcf00fc38e5a39764f2897df10d64e66438
louisj381 <louisj381@gmail.com>
2018-01-20 11:41:33 -0700

    ensured that target directory for clone exists, ensured consistency between scripts

--- PrimaryInstallScript.sh
+++ PrimaryInstallScript.sh
@@ -13,32 +13,35 @@ if [ $? -ne 0 ]; then
 fi
 
 #install Epsilon-Hermes
-git clone https://github.com/UCSolarCarTeam/Epsilon-Hermes.git /opt/
-(cd /opt/Epsilon-Hermes && cd ../ && mv ./Epsilon-Hermes ./src && mkdir Epsilon-Hermes && mv ./src ./Epsilon-Hermes/)
+git clone https://github.com/UCSolarCarTeam/Epsilon-Hermes.git /opt/Epsilon-Hermes
+cd /opt/Epsilon-Hermes && cd ../ && mv ./Epsilon-Hermes ./src && mkdir Epsilon-Hermes && mv ./src ./Epsilon-Hermes/
 qmake /opt/Epsilon-Hermes/
 make /opt/Epsilon-Hermes/
 /opt/build/SchulichEpsilonHermes
+
 git clone https://github.com/alanxz/SimpleAmqpClient /tmp/SimpleAmqpClient/
 mkdir /tmp/SimpleAmqpClient/build
-(cd /tmp/SimpleAmqpClient/build && cmake -DRabbitmqc_INCLUDE_DIR=../../rabbitmq-c/librabbitmq -DRabbitmqc_LIBRARY=../../rabbitmq-c/build/librabbitmq ..)
+(cd /tmp/SimpleAmqpClient/build && cmake \
+	-DRabbitmqc_INCLUDE_DIR=../../rabbitmq-c/librabbitmq \
+	-DRabbitmqc_LIBRARY=../../rabbitmq-c/build/librabbitmq ..)
 (cd /tmp/SimpleAmqpClient/build make)
-sudo mkdir /usr/local/include/SimpleAmqpClient
-sudo cp *.so* /usr/local/lib/
-sudo cp ../src/SimpleAmqpClient/*.h /usr/local/include/SimpleAmqpClient
+mkdir /usr/local/include/SimpleAmqpClient
+cp *.so* /usr/local/lib/
+cp ../src/SimpleAmqpClient/*.h /usr/local/include/SimpleAmqpClient
 
 #install BackupCamera
-git clone https://github.com/UCSolarCarTeam/BackupCamera.git /opt/
+git clone https://github.com/UCSolarCarTeam/BackupCamera.git /opt/BackupCamera/
 /opt/BackupCamera/Installer/MainInstaller.sh
 /opt/BackupCamera/Installer/AutoBootSetup.sh
 tvservice -d edid
 edidparser edid
 
 #install Dashboard
-git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git /opt/
+git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git /opt/Epsilon-Dashboard/
 /opt/Epsilon-Dashboard/EpsilonDashboardSetup.sh
 qmake /opt/Epsilon-Dashboard/src/
 make /opt/Epsilon-Dashboard/src/
 /opt/build/EpsilonDashboard
 
 #install Domovoi
-git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git /opt/
+git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git /opt/Domovoi/
--- SecondaryInstallScript.sh
+++ SecondaryInstallScript.sh
@@ -13,22 +13,25 @@ if [ $? -ne 0 ]; then
 fi
 
 #install BackupCamera
-git clone https://github.com/UCSolarCarTeam/BackupCamera.git /opt/
+git clone https://github.com/UCSolarCarTeam/BackupCamera.git /opt/BackupCamera/
 /opt/BackupCamera/Installer/MainInstaller.sh
 /opt/BackupCamera/Installer/AutoBootSetup.sh
 tvservice -d edid
 edidparser edid
 
 #install Dashboard
-git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git /opt/
+git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git /opt/Epsilon-Dashboard/
 /opt/Epsilon-Dashboard/EpsilonDashboardSetup.sh
+qmake /opt/Epsilon-Dashboard/src/
+make /opt/Epsilon-Dashboard/src/
+/opt/build/EpsilonDashboard
 
 #install Epsilon-Onboard-Media-Control
-git clone https://github.com/UCSolarCarTeam/Epsilon-Onboard-Media-Control.git /opt/
+git clone https://github.com/UCSolarCarTeam/Epsilon-Onboard-Media-Control.git /opt/Epsilon-Onboard-Media-Control/
 qmake /opt/Epsilon-Onboard-Media-Control/
 make /opt/Epsilon-Onboard-Media-Control/
 /opt/build/OnBoardMediaControl
 
 #install Domovoi
-git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git /opt/
+git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git /opt/Domovoi/
 
