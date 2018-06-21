f5fbb2284287dd92cec693c8c9e51c47b47afe37
bill-luu <billluu619@gmail.com>
2018-06-20 18:43:10 -0600

    Changes to Startup.sh to reduce loading time

--- primary/Startup.sh
+++ primary/Startup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-sleep 5
-if pgrep -l "Schulich" > /dev/null
-then
-    echo "Already Running"
-else
-    (export DISPLAY=:0 && cd /home/pi/Desktop/Epsilon-Domovoi && sudo -E ./domovoi.py secondary)
+# Note - When using two screens, it acts as two logins, so the .profile is run twice
+# Be careful to not accidentally get it to run domovoi twice. 
+# First, check if Hermes or Dashboard is running
+if pgrep -l "Epsilon" > /dev/null
+	then
+	    echo "An instance of Hermes or Dashboard is already running."
+	else
+		#Sleep for a random time between 0 and 5 seconds
+		sleep 4s
+		if pgrep -l "Epsilon" > /dev/null
+			then
+				echo "An instance of Hermes or Dashboard is already running."
+	    	else
+	    		rabbitmqctl start_app
+	    		(export DISPLAY=:0 && cd /home/pi/Desktop/Epsilon-Domovoi && sudo -E ./domovoi.py secondary)
+	    fi
 fi
