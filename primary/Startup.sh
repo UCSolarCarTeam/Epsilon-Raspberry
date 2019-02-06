4d2fcc3964f2edda7cf068433563456f023f1f69
bill-luu <billluu619@gmail.com>
2019-02-05 21:04:01 -0700

    Fix indentation of primary startup script

--- primary/Startup.sh
+++ primary/Startup.sh
@@ -4,13 +4,13 @@
 # First, check if Hermes or Dashboard is running
 sleep 6
 start=true
-	if pgrep -l "Epsilon" > /dev/null
-	then
-    		echo "An instance of Hermes or Dashboard is already running."
-		start=false
-	fi
+if pgrep -l "Epsilon" > /dev/null
+then
+	echo "An instance of Hermes or Dashboard is already running."
+	start=false
+fi
 if [ $start = true ];
 then
-echo "Starting programs"
-(export DISPLAY=:0 && cd /home/pi/Desktop/Epsilon-Domovoi && sudo -E ./domovoi.py primary)
+	echo "Starting programs"
+	(export DISPLAY=:0 && cd /home/pi/Desktop/Epsilon-Domovoi && sudo -E ./domovoi.py primary)
 fi
