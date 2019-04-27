b9594727e1d7e0ed9f98299730f2bf1fb33661ab
Bill Luu <billluu619@gmail.com>
2019-04-27 11:22:08 -0600

    dded autoboot program to start the startup script
    We have moved to using the autostart, so we don't have to worry about the script running twice. 
    
    https://developer.toradex.com/knowledge-base/how-to-autorun-application-at-the-start-up-in-linux
    See section: .desktop files
    
    Also updated README with information on .desktop files

--- primary/Startup.sh
+++ primary/Startup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# Note - When using two screens, it acts as two logins, so the .profile is run twice
-# Be careful to not accidentally get it to run domovoi twice. 
-# First, check if Hermes or Dashboard is running
-sleep 6
-start=true
-if pgrep -l "Epsilon" > /dev/null
+if pgrep -l "Schulich" > /dev/null
 then
-	echo "An instance of Hermes or Dashboard is already running."
-	start=false
-fi
-if [ $start = true ];
-then
-	echo "Starting programs"
-	(export DISPLAY=:0 && cd /home/pi/Desktop/Epsilon-Domovoi && sudo -E ./domovoi.py primary)
+    echo "Already Running"
+else
+    export DISPLAY=:0
+    cd /home/pi/Documents/SolarCar/Epsilon-Domovoi && sudo -E ./domovoi.py primary
 fi
