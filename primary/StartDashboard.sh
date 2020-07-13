61c6395d3981d066e1ff22f1366a45aaeea9389b
Bill Luu <billluu619@gmail.com>
2020-07-13 13:32:27 -0600

    SFT-166 Remove references to domovoi and use systemd services
    Systemd services are more robust than .desktop programs, and allow us to add units better and speed up startup. Try to avoid using domovoi and instead handle the pinging of the other pi via a bash script.

---
+++ primary/StartDashboard.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+COMMAND="/opt/SchulichEpsilonDashboard/EpsilonDashboard"
+ARGS="--platform xcb"
+
+SECONDARYPI_ADDRESS="google.com"
+
+ping -q -c1 $SECONDARYPI_ADDRESS > /dev/null
+
+if [ $? -ne 0 ]; then
+	echo "Cannot reach other pi"
+	ARGS="-r --platform xcb"
+fi
+
+($COMMAND $ARGS)
--- secondary/Startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if pgrep -l "Epsilon" > /dev/null
-then
-	echo "running"
-else
-	echo "Starting"
-	(cd /home/pi/Documents/SolarCar/Epsilon-Domovoi && sudo -E ./domovoi.py secondary)
-fi
